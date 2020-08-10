b36c4ab6c74cc1e26ed7e6844383ac65a8c62ef1
John Aven (snoodleboot) <john.aven@hashmapinc.com>
2020-08-09 23:14:53 -0500

    UPDATED package version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="confizzo",
-    version="0.1.0.0",
+    version="0.1.1.0",
     author="John Aven",
     author_email="accelerators@hashmapinc.com",
     description="DO NOT USE - This is a beta package",
